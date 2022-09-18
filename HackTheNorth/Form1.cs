6540b88551cd81ac2b39de38a395de544829b747
demotivate <50751626+demotivate@users.noreply.github.com>
2022-09-18 08:32:26 -0600

    commends

--- HackTheNorth/Form1.cs
+++ HackTheNorth/Form1.cs
@@ -5,9 +5,11 @@ namespace HackTheNorth
 {
     public partial class MainForm : Form
     {
+        // stores strings to be detected and its corresponding mathematical symbol
         Dictionary<string, string> convMaps = new Dictionary<string, string>();
+
+        // builds up a string if program suspects for itself to detect a desired string, then will be used to find symbol in dictionary
         string currWord = "";
-        //int prevLen = 0;
 
         public MainForm()
         {
@@ -16,7 +18,7 @@ namespace HackTheNorth
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            // adds strings and corresponding mathematical symbols to dictionary
             convMaps.Add("pi", "\u03c0");
 
             convMaps.Add("^0", "\u2070");
@@ -54,24 +56,36 @@ namespace HackTheNorth
             convMaps.Add("_9", "\u2089");
         }
 
+
+        /* CALLED WHEN richTextBox1.Text IS CHANGED
+         * HANDLES CONVERTING RAW STRING TO DESIRED SYMBOLS
+         * 
+         * fullText is variable of richTextBox1's text
+         * 
+         * fullText.Length is checked to be of 0 length to prevent index out of range error
+         * 
+         * isSus method is ran and set in a conditional to determine whether a character is suspicious of leading to a mathematical symbol,
+         *  and if so, adds the character to currWord
+         *  
+         * if currWord is not empty, then the program is currently suspecting of a lead to a mathematical symbol, adds current character to
+         *  currWord and is used as index in convMaps.
+         *  
+         * if the string is correspondent to mathematical symbol, add mathematical symbol, else, first add the character that wasn't outputted
+         *  due to suspicion, then, in a general case:
+         *  
+         * add the current character to output
+         */
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
             string fullText = richTextBox1.Text;
             if (fullText.Length == 0)
             {
                 richTextBox2.Text = "";
-                //prevLen = 0;
                 return;
             }
-            //if (prevLen > fullText.Length)
-            //{
-            //    currWord = "";
-            //    return;
-            //}
             if (isSus(fullText))
             {
                 currWord += fullText[fullText.Length - 1];
-                //prevLen = fullText.Length;
                 return;
             }
             if (currWord.Length > 0)
@@ -81,16 +95,20 @@ namespace HackTheNorth
                 {
                     richTextBox2.Text += convMaps[currWord];
                     currWord = "";
-                    //prevLen = fullText.Length;
                     return;
                 }
                 currWord = "";
                 if(fullText.Length >= 2) richTextBox2.Text += fullText[fullText.Length - 2];
             }
-            //prevLen = fullText.Length;
             richTextBox2.Text += fullText[fullText.Length - 1];
         }
 
+        /* CHECKS IF CURRENT CHAR IS SUSPICIOUS OF LEADING TO ONE OF THE DESIRED STRINGS
+         * 
+         * first checks if length of str is 0 to prevent index out of range error
+         * 
+         * returns if the last character of the string matches to anyone of the first characters in the dictionary's strings
+         */
         private bool isSus(string str)
         {
             int len = str.Length;
@@ -111,9 +129,14 @@ namespace HackTheNorth
                 last == 'f';
         }
 
+        /* MAKES SURE richTextBox2.Text IS IN SYNC WITH richTextBox1, ESPECIALLY IF USER BACKSPACES
+         * 
+         * checks if richTextBox1.Text is shorter than richTextBox2.Text (it shouldn't be shorter)
+         * 
+         * if the above is true, remove the last two characters of richTextBox2.Text
+         */
         private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
-            //...
             if (richTextBox1.Text.Length < realTextLength(richTextBox2.Text))
             {
                 string newStr = "";
@@ -125,6 +148,12 @@ namespace HackTheNorth
             }
         }
 
+        /* GET LENGTH OF richTextBox2.Text IF IT WAS THE RAW STRING
+         * 
+         * int res is defined and will be what is returned
+         * 
+         * loops through every character in str, if is one of the math symbols, res = res+2, else, res++
+         */
         private int realTextLength(string str)
         {
             int res = 0;
